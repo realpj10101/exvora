1e8e5b4838f4b90ccc4ed62f8dbdd36fd39808d4
realpj10101 <parsajfr1385oo@gmail.com>
2025-07-09 18:18:54 +0330

    create pagination models and helpers

---
+++ backend/api/Helpers/ExchangeParams.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+
+namespace api.Helpers;
+
+public class ExchangeParams : PaginationParams
+{
+    [MaxLength(300)]
+    public string? Search { get; set; } = string.Empty;
+
+    [MaxLength(30)] public string? OrderBy { get; set; } = string.Empty;
+}

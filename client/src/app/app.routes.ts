54b5625df654e890d662b42b2830eda1424a968e
realpj10101 <parsajfr1385oo@gmail.com>
2025-07-24 00:54:05 +0330

    create login component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/account/register/register.component';
+import { LoginComponent } from './components/account/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'home', component: HomeComponent},
-    { path: 'account/register', component: RegisterComponent}
+    { path: 'account/register', component: RegisterComponent},
+    { path: 'account/login', component: LoginComponent}
 ];
--- client/src/app/components/account/login/login.component.ts
+++ client/src/app/components/account/login/login.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AccountService } from '../../../services/account.service';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Login } from '../../../models/login.model';
+import { LoggedInUser } from '../../../models/logged-in-user.model';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    MatIconModule, MatFormFieldModule,
+    MatButtonModule, MatInputModule, MatDividerModule, RouterModule,
+    FormsModule, ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  private _fb = inject(FormBuilder);
+  private _accountService = inject(AccountService);
 
+  showPassword: boolean = false;
+
+  loginFg = this._fb.group({
+    emailCtrl: ['', [Validators.required, Validators.pattern(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$/), Validators.maxLength(50)]],
+    passwordCtrl: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
+  });
+
+  get EmailCtrl(): FormControl {
+    return this.loginFg.get('emailCtrl') as FormControl;
+  }
+
+  get PasswordCtrl(): FormControl {
+    return this.loginFg.get('passwordCtrl') as FormControl;
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login(): void {
+    console.log('ok');
+
+    let loginPlayer: Login = {
+      email: this.EmailCtrl.value,
+      password: this.PasswordCtrl.value
+    }
+
+    this._accountService.login(loginPlayer).subscribe({
+      next: (loggedInPlayer: LoggedInUser | null) => {
+        console.log(loggedInPlayer);
+      },
+      // show wrong username or password error.
+    })
+  }
 }

edd78a1dd0452ba3bc0dd90342475a6c0e7b3fe3
realpj10101 <parsajfr1385oo@gmail.com>
2025-07-23 23:15:58 +0330

    add designs

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(withEventReplay())
+    provideClientHydration(withEventReplay()),
+    provideHttpClient()
   ]
 };
--- client/src/app/components/account/register/register.component.ts
+++ client/src/app/components/account/register/register.component.ts
@@ -4,11 +4,18 @@ import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, 
 import { MatIconModule } from '@angular/material/icon';
 import { AccountService } from '../../../services/account.service';
 import { Register } from '../../../models/register.model';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-register',
   imports: [
-    FormsModule, ReactiveFormsModule, CommonModule, MatIconModule
+    FormsModule, ReactiveFormsModule, CommonModule,
+    MatIconModule, MatSelectModule, MatInputModule,
+     MatFormFieldModule, MatButtonModule, MatDividerModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
@@ -24,16 +31,21 @@ export class RegisterComponent {
   acceptTerms: boolean = false;
   acceptMarketing: boolean = false;
   passwordStrengthValue: number = 0;
+  passwrodsNotMatch: boolean | undefined;
+  countryOptions: string[] = ['United States', 'Unites Kingdom', 'Canada', 'Australia', 'Germanay', 'France', 'Japan', 'Iran', 'Other']
 
   registerFg = this._fb.group({
     firstNameCtrl: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
     lastNameCtrl: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(50)]],
-    emailCtrl: ['', [Validators.required, Validators.email, Validators.maxLength(50)]],
+    emailCtrl: ['', [Validators.required, Validators.pattern(/^([\w\.\-]+)@([\w\-]+)((\.(\w){2,5})+)$/), Validators.maxLength(50)]],
     phoneNumberCtrl: ['', [Validators.required]],
-    countryCtrl: ['', [Validators.required]],
-    passwordCtrl: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
-    confirmPasswordCtrl: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]]
+    countryCtrl: ['United States', [Validators.required]],
   });
+
+  securityFg = this._fb.group({
+    passwordCtrl: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
+    confirmPasswordCtrl: ['', [Validators.required]]
+  })
 
   get FirstNameCtrl(): FormControl {
     return this.registerFg.get('firstNameCtrl') as FormControl;
@@ -56,16 +68,37 @@ export class RegisterComponent {
   }
 
   get PasswordCtrl(): FormControl {
-    return this.registerFg.get('passwordCtrl') as FormControl;
+    return this.securityFg.get('passwordCtrl') as FormControl;
   }
 
   get ConfirmPasswordCtrl(): FormControl {
-    return this.registerFg.get('confirmPasswordCtrl') as FormControl;
+    return this.securityFg.get('confirmPasswordCtrl') as FormControl;
+  }
+
+  get Password(): string {
+    return this.PasswordCtrl.value || '';
+  }
+
+  get IsLongEnough(): boolean {
+    return this.Password.length >= 8;
+  }
+
+  get HasUppercase(): boolean {
+    return /[A-Z]/.test(this.Password);
+  }
+
+  get HasNumber(): boolean {
+    return /[0-9]/.test(this.Password);
+  }
+
+  get HasSpecialChar(): boolean {
+    return /[!@#$%^&*(),.?":{}|<>]/.test(this.Password);
   }
 
   nextStep() {
-    if (this.registerFg.valid) 
-      this.step = 2
+    if (this.registerFg.valid)
+      console.log('ok');
+    this.step = 2
   }
 
   prevStep() {
@@ -94,20 +127,58 @@ export class RegisterComponent {
     if (password.length >= 8) strength++;
     if (/[A-Z]/.test(password)) strength++
     if (/[0-9]/.test(password)) strength++
+    if (/[!@#$%^&*(),.?":{}|<>]/.test(password)) strength++;
     return strength;
   }
 
-  register(): void {
-    let userInput: Register = {
-      firstName: this.FirstNameCtrl.value,
-      lastName: this.LastNameCtrl.value,
-      email: this.EmailCtrl.value,
-      phoneNumber: this.PhoneNumberCtrl.value,
-      country: this.CountryCtrl.value,
-      password: this.PasswordCtrl.value,
-      confirmPassword: this.ConfirmPasswordCtrl.value
+  getStrengthColor(strength: number): string {
+    switch (strength) {
+      case 0:
+      case 1:
+        return 'weak';
+      case 2:
+      case 3:
+        return 'medium';
+      case 4:
+        return 'strong';
+      default:
+        return 'none';
     }
+  }
 
-    this._accountService.register(userInput).subscribe();
+  getStrengthText(strength: number): string {
+    switch (strength) {
+      case 0:
+      case 1:
+        return 'Weak';
+      case 2:
+      case 3:
+        return 'Medium';
+      case 4:
+        return 'Strong';
+      default:
+        return '';
+    }
+  }
+
+  register(): void {
+    if (this.PasswordCtrl.value === this.ConfirmPasswordCtrl.value) {
+      this.passwrodsNotMatch = false;
+
+      let userInput: Register = {
+        firstName: this.FirstNameCtrl.value,
+        lastName: this.LastNameCtrl.value,
+        email: this.EmailCtrl.value,
+        phoneNumber: this.PhoneNumberCtrl.value,
+        country: this.CountryCtrl.value,
+        password: this.PasswordCtrl.value,
+        confirmPassword: this.ConfirmPasswordCtrl.value
+      }
+
+      this._accountService.register(userInput).subscribe();
+    }
+    else {
+      this.passwrodsNotMatch = true;
+    }
   }
 }

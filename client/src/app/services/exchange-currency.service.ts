93effb20b17448aaa3f3a8a76400b7f11acf5404
realpj10101 <parsajfr1385oo@gmail.com>
2025-07-29 16:23:07 +0330

    crearte exchange currency service

--- client/src/app/services/exchange-currency.service.ts
+++ client/src/app/services/exchange-currency.service.ts
@@ -1,8 +1,43 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../environments/environment.development';
+import { ExchangeCurrencyReq, ExchangeCurrencyRes } from '../models/exchange-currency.model';
+import { Observable } from 'rxjs';
+import { ApiResponse } from '../models/apiResponse.model';
+import { PaginationHandler } from '../extensions/paginationHandler';
+import { ExchangeParams } from '../models/helpers/exchange-params.model';
+import { PaginatedResult } from '../models/helpers/pagination-result.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExchangeCurrencyService {
-  
+  private _http = inject(HttpClient);
+
+  private readonly _baseApiUrl = environment.apiUrl + 'api/exchangecurrency/';
+  private readonly _paginationHandler = new PaginationHandler();
+
+  AddExchangeCurrency(request: ExchangeCurrencyReq): Observable<ApiResponse> {
+    return this._http.post<ApiResponse>(this._baseApiUrl + 'add-currency', request);
+  }
+
+  getAll(exchangeParams: ExchangeParams, exchangeName: string): Observable<PaginatedResult<ExchangeCurrencyRes[]>> {
+    const params = this.getHttpParams(exchangeParams);
+
+    return this._paginationHandler.getPaginatedResult<ExchangeCurrencyRes[]>(this._baseApiUrl + 'get-currency/' + exchangeName, params);
+  }
+
+  private getHttpParams(exchangeParams: ExchangeParams): HttpParams {
+    let params = new HttpParams();
+
+    if (exchangeParams) {
+      if (exchangeParams.search)
+        params = params.append('search', exchangeParams.search);
+      
+      params = params.append('pageSize', exchangeParams.pageSize);
+      params = params.append('pageNumber', exchangeParams.pageNumber);
+    }
+    
+    return params;
+  }
 }

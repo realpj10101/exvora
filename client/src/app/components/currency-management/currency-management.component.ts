cebfa72a5001b720291bb33f083f895a07107a6e
realpj10101 <parsajfr1385oo@gmail.com>
2025-08-03 14:19:31 +0330

    create add currency

---
+++ client/src/app/components/currency-management/currency-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-currency-management',
+  imports: [],
+  templateUrl: './currency-management.component.html',
+  styleUrl: './currency-management.component.scss'
+})
+export class CurrencyManagementComponent {
+
+}
--- client/src/app/services/currency.service.ts
+++ client/src/app/services/currency.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { CurrencyRes } from '../models/currency.model';
+import { CurrencyReq, CurrencyRes } from '../models/currency.model';
 import { environment } from '../environments/environment.development';
 
 @Injectable({
@@ -11,6 +11,10 @@ export class CurrencyService {
   private _http = inject(HttpClient);
   
   private readonly _baseApiUrl = environment.apiUrl + 'api/currency';
+
+  addCurrency(request: CurrencyReq): Observable<CurrencyRes> {
+    return this._http.post<CurrencyRes>(this._baseApiUrl + 'add-currency', request);
+  }
 
   getAll(): Observable<CurrencyRes[]> {
     return this._http.get<CurrencyRes[]>(this._baseApiUrl);

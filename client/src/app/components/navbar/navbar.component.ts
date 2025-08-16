1fb9a3eb89263462d1a123152f050bb1570c6fd4
realpj10101 <parsajfr1385oo@gmail.com>
2025-08-16 16:50:54 +0330

    fix navbar ui

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { AccountService } from '../../services/account.service';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    MatIconModule, RouterModule
+    MatIconModule, RouterModule, CommonModule
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
